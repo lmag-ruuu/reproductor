7befefe5cad53ad88c5fc187419f051a8c39adaa
Daarin <sempaipotato@gmail.com>
2021-08-17 08:09:52 -0400

    barra de progreso al js

--- js/reproductor.js
+++ js/reproductor.js
@@ -33,9 +33,36 @@ listadoCanciones.onclick = e => {
   indice[0] = e.target.id;
   cambiarIconoxD();
 };
-
+//El "xD" es porque no es un icono pero no supe como llamarlo xd
 const cambiarIconoxD = () => {
   const elemento = document.getElementById("playText");
   let contenido = elemento.textContent;
   elemento.textContent = contenido === "Play" ? "Pause" : "Play";
+};
+
+const volumen = document.getElementById('vol');
+volumen.oninput = e => {
+  const vol = e.target.value;
+  player.volume = vol;
+};
+
+//La "barra" de progreso de la cancion òwó
+const updateProgress = () => { 
+  if(player.currentTime > 0) {
+    let duracion, duracionSegundos, dura, actualSegundos, actual;
+    const barra = document.getElementById('progreso');
+    barra.value = (player.currentTime / player.duration) * 100;
+
+    duracionSegundos = player.duration.toFixed(0);
+    dura = secondsToString(duracionSegundos);
+    actualSegundos = player.currentTime.toFixed(0);
+    actual = secondsToString(actualSegundos);
+
+    duracion = `${actual}/${dura}`;
+    document.getElementById('tiempo').innerText = duracion;
+
+  }
+  if(player.ended){
+    siguienteCancion();
+  }
 };
